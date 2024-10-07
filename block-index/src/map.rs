5f5444a408bbc435091f975540f7228307a872e7
zolting <choinieredillan@gmail.com>
2024-10-07 18:49:02 -0400

    Retain txs and tx_results

--- block-index/src/map.rs
+++ block-index/src/map.rs
@@ -2,20 +2,35 @@ use substreams_cosmos::Block;
 
 #[substreams::handlers::map]
 fn map_blocks(params: String, mut block: Block) -> Result<Block, substreams::errors::Error> {
-    // TO-DO: apply retain also on `txs`
-    // https://github.com/pinax-network/subgraph-cosmos-proposals/issues/8
-    block.tx_results.retain(|tx_result| {
-        if tx_result.code != 0 {
-            return false;
+    // Filter both tx_results and txs based on the same criteria
+    let mut retained_indices = Vec::new();
+
+    for (index, tx_result) in block.tx_results.iter().enumerate() {
+        if tx_result.code == 0
+            && tx_result.events.iter().any(|event| {
+                event.r#type == "submit_proposal"
+                    || event.r#type == "proposal_vote"
+                    || event.r#type == "proposal_deposit"
+            })
+        {
+            retained_indices.push(index);
         }
-        // TO-DO: apply retain using `params`
-        // https://github.com/pinax-network/subgraph-cosmos-proposals/issues/4
-        tx_result.events.iter().any(|event| {
-            event.r#type == "submit_proposal" || event.r#type == "proposal_vote" || event.r#type == "proposal_deposit"
-        })
-        // TO-DO: Apply filtering based on `events.attr`
-        // https://github.com/pinax-network/subgraph-cosmos-proposals/issues/9
-    });
+    }
+
+    // Retain tx_results
+    block.tx_results = retained_indices
+        .iter()
+        .map(|&index| block.tx_results[index].clone())
+        .collect();
+
+    // Retain txs
+    block.txs = retained_indices.iter().map(|&index| block.txs[index].clone()).collect();
+
+    // TO-DO: apply retain using `params`
+    // https://github.com/pinax-network/subgraph-cosmos-proposals/issues/4
+
+    // TO-DO: Apply filtering based on `events.attr`
+    // https://github.com/pinax-network/subgraph-cosmos-proposals/issues/9
 
     Ok(block)
 }
