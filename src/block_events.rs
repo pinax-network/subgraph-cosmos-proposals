1fe6eea6c5250ea750edad61316034da2745cdb3
zolting <choinieredillan@gmail.com>
2024-10-09 16:52:23 -0400

    Add proposal_dropped status update

--- src/block_events.rs
+++ src/block_events.rs
@@ -5,13 +5,21 @@ use crate::utils::get_attribute_value;
 
 pub fn push_block_events(block: &Block, tables: &mut Tables) {
     for event in block.events.iter() {
-        if event.r#type == "active_proposal" {
-            if let (Some(proposal_id), Some(status)) = (
-                get_attribute_value(event, "proposal_id"),
-                get_attribute_value(event, "proposal_result"),
-            ) {
-                tables.update_row("Proposal", proposal_id).set("status", status);
-            }
+        if is_proposal_event(event) {
+            update_proposal_status(event, tables);
         }
     }
+}
+
+fn update_proposal_status(event: &substreams_cosmos::pb::Event, tables: &mut Tables) {
+    if let (Some(proposal_id), Some(status)) = (
+        get_attribute_value(event, "proposal_id"),
+        get_attribute_value(event, "proposal_result"),
+    ) {
+        tables.update_row("Proposal", proposal_id).set("status", status);
+    }
+}
+
+fn is_proposal_event(event: &substreams_cosmos::pb::Event) -> bool {
+    event.r#type == "active_proposal" || event.r#type == "inactive_proposal"
 }
