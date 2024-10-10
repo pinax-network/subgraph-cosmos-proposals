26e96ea821c2e8ef090aab29772fe511c22bea6a
zolting <choinieredillan@gmail.com>
2024-10-10 10:48:06 -0400

    Fix insertion errors

--- src/block_events.rs
+++ src/block_events.rs
@@ -16,10 +16,27 @@ fn update_proposal_status(event: &substreams_cosmos::pb::Event, tables: &mut Tab
         get_attribute_value(event, "proposal_id"),
         get_attribute_value(event, "proposal_result"),
     ) {
-        tables.update_row("Proposal", proposal_id).set("status", status);
+        let frm_status = format_proposal_status(&status);
+        tables.update_row("Proposal", proposal_id).set("status", frm_status);
     }
 }
 
 fn is_proposal_event(event: &substreams_cosmos::pb::Event) -> bool {
-    event.r#type == "active_proposal" || event.r#type == "inactive_proposal"
+    matches!(
+        event.r#type.as_str(),
+        "active_proposal" | "inactive_proposal" | "cancel_proposal"
+    )
+}
+
+fn format_proposal_status(status: &str) -> String {
+    match status {
+        "proposal_passed" => "Passed".to_string(),
+        "proposal_rejected" => "Rejected".to_string(),
+        "proposal_dropped" => "Dropped".to_string(),
+        "proposal_failed" => "Failed".to_string(),
+        "proposal_canceled" => "Canceled".to_string(),
+        "expedited_proposal_rejected" => "Rejected".to_string(),
+        "optimistic_proposal_rejected" => "Rejected".to_string(),
+        _ => status.to_string(),
+    }
 }
--- src/blocks.rs
+++ src/blocks.rs
@@ -6,13 +6,12 @@ use crate::utils::to_date;
 pub fn create_block(tables: &mut Tables, clock: &Clock) {
     // timestamp
     let timestamp = clock.timestamp.as_ref().expect("missing timestamp");
-    let seconds = timestamp.seconds;
     let date = to_date(clock);
     let block_number = clock.number.to_string();
 
     tables
         .create_row("Block", clock.id.as_str())
         .set_bigint("number", &block_number.to_string())
-        .set_bigint("timestamp", &seconds.to_string())
+        .set("timestamp", timestamp)
         .set("date", date);
 }
