e096678bfee54cc2cec9d82a412156ea931b7583
Denis Carriere <carriere.denis@gmail.com>
2024-10-09 22:41:04 -0400

    add ParameterChange to graphql

--- src/proposals/parameter_changes.rs
+++ src/proposals/parameter_changes.rs
@@ -5,8 +5,9 @@ use substreams_entity_change::tables::Tables;
 pub fn create_parameter_change_proposal(tables: &mut Tables, content: &Any, proposal_id: &str) {
     if let Ok(msg) = <ParameterChangeProposal as prost::Message>::decode(content.value.as_slice()) {
         msg.changes.iter().for_each(|change| {
+            let key = format!("{}:{}:{}", proposal_id, change.subspace, change.key);
             tables
-                .create_row("ParameterChange", &proposal_id)
+                .create_row("ParameterChange", &key)
                 .set("subspace", change.subspace.as_str())
                 .set("key", change.key.as_str())
                 .set("value", change.value.as_str())
