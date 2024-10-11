7fccb9c16c2f20d29bdbd2717d13ba6600f7a976
Denis Carriere <carriere.denis@gmail.com>
2024-10-11 11:53:03 -0400

    Refactor SoftwareUpgrade fixes: https://github.com/pinax-network/subgraph-cosmos-proposals/issues/19

--- src/proposals/software_upgrades.rs
+++ src/proposals/software_upgrades.rs
@@ -1,26 +1,26 @@
 use prost_types::Any;
 use substreams_entity_change::tables::Tables;
 
-use crate::pb::cosmos::upgrade::v1beta1::{MsgSoftwareUpgrade, SoftwareUpgradeProposal};
+use crate::pb::cosmos::upgrade::v1beta1::{MsgSoftwareUpgrade, Plan, SoftwareUpgradeProposal};
+
+pub fn create_row_for_upgrade(tables: &mut Tables, proposal_id: &str, plan: Plan) {
+    tables
+        .create_row("SoftwareUpgradeProposal", &proposal_id)
+        .set("name", plan.name.as_str())
+        .set("height", plan.height)
+        .set("info", plan.info.as_str())
+        .set("proposal", proposal_id);
+}
 
 pub fn create_software_upgrade(tables: &mut Tables, content: &Any, proposal_id: &str) {
-    if let Ok(msg_software_upgrade) = <MsgSoftwareUpgrade as prost::Message>::decode(content.value.as_slice()) {
-        let plan = msg_software_upgrade.plan.expect("missing software upgrade plan");
-        tables
-            .create_row("SoftwareUpgradeProposal", &proposal_id)
-            .set("planName", plan.name.as_str())
-            .set("planHeight", plan.height)
-            .set("planInfo", plan.info.as_str())
-            .set("proposal", proposal_id);
+    if let Ok(msg) = <MsgSoftwareUpgrade as prost::Message>::decode(content.value.as_slice()) {
+        let plan = msg.plan.expect("missing software upgrade plan");
+        create_row_for_upgrade(tables, proposal_id, plan);
     }
+
     // Deprecated: This legacy proposal is deprecated in favor of Msg-based gov
-    if let Ok(msg_software_upgrade) = <SoftwareUpgradeProposal as prost::Message>::decode(content.value.as_slice()) {
-        let plan = msg_software_upgrade.plan.expect("missing software upgrade plan");
-        tables
-            .create_row("SoftwareUpgradeProposal", &proposal_id)
-            .set("planName", plan.name.as_str())
-            .set("planHeight", plan.height)
-            .set("planInfo", plan.info.as_str())
-            .set("proposal", proposal_id);
+    if let Ok(msg) = <SoftwareUpgradeProposal as prost::Message>::decode(content.value.as_slice()) {
+        let plan = msg.plan.expect("missing software upgrade plan");
+        create_row_for_upgrade(tables, proposal_id, plan);
     }
 }
