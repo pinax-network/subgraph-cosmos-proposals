c644ed856b4cd17420ae7803ea6eecb69f61f33b
zolting <44510235+zolting@users.noreply.github.com>
2024-11-08 13:28:40 -0500

    Add gov-params module handling of expedited governance parameters

--- cosmos-governance-parameters/src/gov_params.rs
+++ cosmos-governance-parameters/src/gov_params.rs
@@ -32,13 +32,27 @@ pub fn gov_params(
 }
 
 fn set_new_gov_params(gov_params: &StoreSetString, gov_param_proposal_str: &String) {
-    let parsed: GovParamsOptional = serde_json::from_str(&gov_param_proposal_str).expect("Failed to parse gov params");
+    // Cosmos SDK uses "veto" instead of "veto_threshold" in the JSON
+    // Injective also uses "veto" instead of "veto_threshold", though in genesis file it's "veto_threshold"
+    // Osmosis uses "min_expedited_deposit" instead of "expedited_min_deposit"
+    // https://docs.injective.network/developers/modules/core/gov#parameters
+    // https://docs.osmosis.zone/osmosis-core/modules/gov
+    let formatted_str = gov_param_proposal_str
+        .replace("veto", "veto_threshold")
+        .replace("min_expedited_deposit", "expedited_min_deposit");
+
+    let parsed: GovParamsOptional = serde_json::from_str(&formatted_str).expect("Failed to parse gov params");
 
     if let Some(deposit_params) = parsed.deposit_params {
         if deposit_params.min_deposit.len() > 0 {
             let min_deposit_str: String =
                 serde_json::to_string(&deposit_params.min_deposit).expect("Failed to serialize min deposit");
             gov_params.set(0, "min_deposit", &min_deposit_str);
+        }
+        if deposit_params.expedited_min_deposit.len() > 0 {
+            let expedited_min_deposit_str: String = serde_json::to_string(&deposit_params.expedited_min_deposit)
+                .expect("Failed to serialize expedited min deposit");
+            gov_params.set(0, "expedited_min_deposit", &expedited_min_deposit_str);
         }
         if let Some(max_deposit_period) = deposit_params.max_deposit_period {
             gov_params.set(0, "max_deposit_period", &max_deposit_period);
@@ -49,14 +63,23 @@ fn set_new_gov_params(gov_params: &StoreSetString, gov_param_proposal_str: &Stri
         if let Some(voting_period) = voting_params.voting_period {
             gov_params.set(0, "voting_period", &voting_period);
         }
+        if let Some(expedited_voting_period) = voting_params.expedited_voting_period {
+            gov_params.set(0, "expedited_voting_period", &expedited_voting_period);
+        }
     }
 
     if let Some(tally_params) = parsed.tally_params {
         if let Some(quorum) = tally_params.quorum {
             gov_params.set(0, "quorum", &quorum);
         }
+        if let Some(expedited_quorum) = tally_params.expedited_quorum {
+            gov_params.set(0, "expedited_quorum", &expedited_quorum);
+        }
         if let Some(threshold) = tally_params.threshold {
             gov_params.set(0, "threshold", &threshold);
+        }
+        if let Some(expedited_threshold) = tally_params.expedited_threshold {
+            gov_params.set(0, "expedited_threshold", &expedited_threshold);
         }
         if let Some(veto_threshold) = tally_params.veto_threshold {
             gov_params.set(0, "veto_threshold", &veto_threshold);
